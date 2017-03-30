480a7db3eb227e896d786a832d5f14ac49dbd478
Aknos706 <aknos706@gmail.com>
2017-03-30 23:33:34 +0200

    Blueprint Class

---
+++ EVI-RE/src/util/SkillWrapper.java
@@ -0,0 +1,9 @@
+package util;
+
+public class SkillWrapper extends Wrapper_Base{
+    public int skillLevel=0;
+    public SkillWrapper(int id,int lvl){
+        super(id);
+        skillLevel=lvl;
+    }
+}
