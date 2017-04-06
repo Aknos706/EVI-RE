7d889a68fff980a26c98c30fa3789287bbf8d75a
Aknos706 <aknos706@gmail.com>
2017-04-06 15:28:23 +0200

    cleanup

--- EVI-RE/src/util/Wrapper_Base.java
+++ EVI-RE/src/util/Wrapper_Base.java
@@ -2,16 +2,19 @@ package util;
 
 import Handler.SQL_StaticDataExport;
 
-abstract class Wrapper_Base {
-    public String Name="";
+public abstract class Wrapper_Base {
+    
+    public String typeName="";
     public int typeID=0;
     protected static final SQL_StaticDataExport DATA  = new SQL_StaticDataExport();
+    
     protected Wrapper_Base(int I){  
         this.typeID=I;
-        this.Name=DATA.typeID_to_typeName(I);
+        this.typeName=DATA.typeID_to_typeName(I);
     }
+    
     protected Wrapper_Base(String S){
-        this.Name=S;
-        this.typeID=DATA.typeName_to_typeID(Name);
+        this.typeName=S;
+        this.typeID=DATA.typeName_to_typeID(typeName);
     }
 }
