c996084d71449e511433865aecb0fd0ed639a3f1
aknos706 <aknos706@gmail.com>
2017-04-07 16:13:56 +0200

    WarehouseStorage - Managing the Storage
    Location -< Storage >- Item

--- EVI-RE/src/WIS/WarehouseStorage.java
+++ EVI-RE/src/WIS/WarehouseStorage.java
@@ -1,7 +1,30 @@
 package WIS;
 
 import java.util.HashMap;
+import java.math.BigDecimal;
 
 public class WarehouseStorage {
+    HashMap<Integer, WarehouseItem> StorageMap; // TypeID -> WarehouseItem
+    
+    public WarehouseStorage(){
+        this.StorageMap = new HashMap<>();
+    }
+    
+    public void addToStorage(int tid,int q,BigDecimal v){
+        if(this.StorageMap.containsKey(tid)){
+            WarehouseItem temp = this.StorageMap.get(tid);
+            temp.addQuantity(q, v);
+            this.StorageMap.replace(tid, temp);
+        }else{
+            this.StorageMap.put(tid,new WarehouseItem(tid,q,v));
+        }
+    }
+    
+    public void addToStorage(String typeName,int q,BigDecimal v){
+        
+    }
+    
+    //remove from storage returns nothing
+    //take from storage returns warehouseitem
     
 }
